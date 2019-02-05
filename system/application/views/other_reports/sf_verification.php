aa552b307b39fb03f37b075bc4942af8dca94bbe
keprom <kokshetau.energo.prom@mail.ru>
2019-02-05 15:02:55 +0600

    added total sum in sf verification

--- system/application/views/other_reports/sf_verification.php
+++ system/application/views/other_reports/sf_verification.php
@@ -23,6 +23,11 @@
     </tr>
     </thead>
     <tbody>
+    <?php
+    $itog_kvt = 0;
+    $sum_snds = 0;
+    $sum_beznds = 0;
+    ?>
     <?php foreach ($report as $r): ?>
         <tr>
             <td><?php echo $r->dog; ?></td>
@@ -33,8 +38,23 @@
             <td class="td-number"><?php echo $r->snds; ?></td>
             <td class="td-number"><?php echo $r->nomer; ?></td>
         </tr>
+        <?php
+        $itog_kvt += $r->kvt;
+        $sum_snds += $r->beznds;
+        $sum_beznds += $r->snds;
+        ?>
     <?php endforeach; ?>
     </tbody>
+    <tfoot>
+    <tr>
+        <th colspan="2">ИТОГО</th>
+        <th class="th-number"><?php echo $itog_kvt; ?></th>
+        <th></th>
+        <th class="th-number"><?php echo $sum_snds; ?></th>
+        <th class="th-number"><?php echo $sum_beznds; ?></th>
+        <th></th>
+    </tr>
+    </tfoot>
 </table>
 
 </body>
